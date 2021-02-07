c77bcddf137f0d85a9173bed6773f707355f1dac
TK <58408265+Tklotz23@users.noreply.github.com>
2021-02-06 20:17:38 -0700

    Update RK4_VF_Solver_Plotter.py

--- RK4_VF_Solver_Plotter.py
+++ RK4_VF_Solver_Plotter.py
@@ -11,6 +11,7 @@ if test_case == 'y':
         ## Test case
     t_0 = 0 # Initial conditions  
     X_0 = np.array([0,1])
+    T = 14*np.pi # End of desired solution interval 
     def RHS_ODE(t,x):
         # alpha = 0.5 # specific parameters for Duffing Equation. 
         # beta = 2/3
@@ -29,10 +30,12 @@ else:
     user_IC_t0 = input("Enter the t_0 value: ")
     user_IC_X01 = input("Enter the 1st X_0 value: ")
     user_IC_X02 = input("Enter the 2nd X_0 value: ")
-
+    user_T = input("Enter the upper bound for the t-interval: ")
+    
     ## Example ODE: Duffing Equation.
     t_0 = float(user_IC_t0) # Initial conditions  
     X_0 = np.array([float(user_IC_X01),float(user_IC_X02)])
+    T = float(user_T)
     def RHS_ODE(t,x):
         # alpha = 0.5 # specific parameters for Duffing Equation. 
         # beta = 2/3
@@ -50,17 +53,22 @@ h = 0.005 # Step-size
 def x_sol(time, X_naught):
     x = X_naught # Initialize I.C.
     sol =[]
+    overflow_risk = False
     for s in time:
-        k1 = RHS_ODE(s,x) # Setting up the RK recursions
-        k2 = RHS_ODE(s+h/2,np.add(x,(h/2)*k1))
-        k3 = RHS_ODE(s+h/2,np.add(x,(h/2)*k2))
-        k4 = RHS_ODE(s+h,np.add(x,h*k3))
-        x = x+(h/6)*(k1+2*k2+2*k3+k4)
-        sol.append(np.array(x).tolist()) # Solution!
+        if not overflow_risk:
+            k1 = RHS_ODE(s,x) # Setting up the RK recursions
+            k2 = RHS_ODE(s+h/2,np.add(x,(h/2)*k1))
+            k3 = RHS_ODE(s+h/2,np.add(x,(h/2)*k2))
+            k4 = RHS_ODE(s+h,np.add(x,h*k3))
+            x = x+(h/6)*(k1+2*k2+2*k3+k4)
+            for x_comp in x:
+                if np.abs(x_comp) >=1E+30:
+                    overflow_risk = True
+                else: 
+                    sol.append(np.array(x).tolist()) # Solution!
     return np.array(sol)
 
-T = 14*np.pi # End of desired solution interval 
-t = np.arange(t_0, T+h, h) # Solution interval. 
+t = np.arange(t_0, T+h, h) # Solution interval.  
 
 ## Function that creates each little line on a mesh_grid according the vector field defined by the ODE system. 
 def vector_line(t,p_x1,p_x2,mesh_r):
@@ -81,17 +89,29 @@ def vector_line(t,p_x1,p_x2,mesh_r):
     circle_pt_anti = np.array([x1_circle_anti,x2_circle_anti]) # Antipodal point on circle. 
     arrow_start = point+circle_pt_anti # Starting end of line.
     arrow_end = point+circle_pt # Ending end of line.
-    arrow = np.array([arrow_start,arrow_end]) # The line! (Or data for matplotlin to plot the line. Needs to be transposed upon function return for correct plot). 
-    return np.transpose(arrow)[0],np.transpose(arrow)[1],angle,arrow_start,point,arrow_end, circle_pt
+    arrow = np.array([arrow_start,arrow_end]) # The line! (Or data for matplotlib to plot the line. Needs to be transposed upon function return for correct plot). 
+    return np.transpose(arrow)[0],np.transpose(arrow)[1],angle,arrow_start,point,arrow_end, circle_pt, arrow
 ## Function to determine color of vectors/lines based on vector magnitude.
 #def line_color(t,p_x1,p_x2,mag_max):
     
-## Now to make a meshgrid adapted to the size of the solution curve
+## Now to make a SQUARE meshgrid adapted to the size of the solution curve
 mesh_density = 30
-x1_val = np.linspace(np.min(x_sol(t,X_0)[:,0]),np.max(x_sol(t,X_0)[:,0]),mesh_density)
-x2_val = np.linspace(np.min(x_sol(t,X_0)[:,1]),np.max(x_sol(t,X_0)[:,1]),mesh_density)
-# m_radius = np.min(np.array([(x1_val[1]-x1_val[0])/2,(x2_val[1]-x2_val[0])/2]))
-m_radius = np.max(np.array([(x1_val[1]-x1_val[0])/2,(x2_val[1]-x2_val[0])/2])) # Half the distance between horizontal or vertical meshpoints
+
+x1_sol_min = np.min(x_sol(t,X_0)[:,0])
+x1_sol_max = np.max(x_sol(t,X_0)[:,0])
+
+x2_sol_min = np.min(x_sol(t,X_0)[:,1])
+x2_sol_max = np.max(x_sol(t,X_0)[:,1])
+
+sq_mesh_size =np.max(np.array([x1_sol_max-x1_sol_min,x2_sol_max-x2_sol_min]))
+
+if x1_sol_max-x1_sol_min >= x2_sol_max-x2_sol_min:
+    x1_val = np.linspace(x1_sol_min,x1_sol_max,mesh_density)
+    x2_val = np.linspace(x1_sol_min,x1_sol_max,mesh_density)
+else:
+    x1_val = np.linspace(x2_sol_min,x2_sol_max,mesh_density)
+    x2_val = np.linspace(x2_sol_min,x2_sol_max,mesh_density)
+m_radius = np.min(np.array([(x1_val[1]-x1_val[0])/2,(x2_val[1]-x2_val[0])/2])) # Half the distance between horizontal or vertical meshpoints
 print(2*m_radius)
 m_x1, m_x2 = np.meshgrid(x1_val,x2_val)
 mesh_full = np.array([m_x1,m_x2]) # Full mesh.
@@ -103,30 +123,8 @@ plt.style.use('dark_background')
 fig, ax = plt.subplots(figsize=(8,8)) # Fixing a square scaling
 ax.plot(x_sol(t,X_0)[:,0],x_sol(t,X_0)[:,1], color='tab:orange') # Solution curve plot
 ax.plot(m_x1,m_x2, marker='.', color='w', linestyle='none') # Plotting the meshgrid
-counter = 0
-min_list = []
-find_bad_len = []
 for p in x1_val: # This double for-loop plots each arrow on the meshgrid.
     for q in x2_val:
         ax.plot(vector_line(t,p,q,m_radius)[0],vector_line(t,p,q,m_radius)[1], color='c')
-        #ax.plot(vector_line(t,p,q,m_radius)[0],vector_line(t,p,q,m_radius)[1],marker='.', color='r',linestyle='none')
-        # The following is a print of a bunch of stuff from the vector_line function. 
-        counter+=1
-        line_length_half = np.linalg.norm(np.transpose(vector_line(t,p,q,m_radius)[9])[1]-np.transpose(vector_line(t,p,q,m_radius)[9])[0])/2 
-        print("counting the point: ",counter)
-        if line_length_half >= 1.05*m_radius:
-            find_bad_len.append(line_length_half)
-        min_list.append(vector_line(t,p,q,m_radius)[1].tolist())
-        print("Length of line...supposedly: ",line_length_half)
-        print("angle: ",vector_line(t,p,q,m_radius)[2])
-        print("start point of line, grid point, and end poit ofline resp.: ",vector_line(t,p,q,m_radius)[3],', ',vector_line(t,p,q,m_radius)[4],', ',vector_line(t,p,q,m_radius)[5])
-        print("point on circle of radius m_radius if centered at origin: ",vector_line(t,p,q,m_radius)[6])
-## this bit is double-checking that the points that have been calculated are what matplotlib thinks they are. 
-line_min = np.min(np.array(min_list))
-print(line_min)
-print(np.min(x2_val))
-print(np.min(x2_val)-m_radius)
-ax.axhline(y=line_min, color='g')
-print(find_bad_len)
 ## Show-off time I guess
 plt.show(fig)
